d5d69c400f40366b77c9a0d994d411cac21377e7
pzivick <pzivick@gmail.com>
2022-06-10 14:10:41 -0500

    Create the file that will contain the necessary functions.

---
+++ model_functions.py
@@ -0,0 +1,15 @@
+'''
+model_functions.py
+
+Creator: Paul Zivick
+Last edited: 6/10/2022
+
+Notes/Description: This is a collection of functions
+that are needed in order to run the Make_predictions
+Jupyter notebook. These functions are adopted from earlier
+versions written for the Zivick+2021 analysis, in particular
+updating to use column names in manipulations rather than
+explicitly stated indice variables.
+'''
+
+import numpy as np
